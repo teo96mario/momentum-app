388700f5baa033f2728b1aa7ae3dadd1befd2717
teo96mario <66203884+teo96mario@users.noreply.github.com>
2026-07-15 12:01:26 +0300

    sw: bump to v3, pre-cache landing.html

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'momentum-v2';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'momentum-v3';
+const ASSETS = ['/', '/index.html', '/landing.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
